782196e8439dd1661c1987975ef78bb46cf50f72
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-11-11 20:11:52 +0300

    Route (Episode 27)

---
+++ src/views/HelpView.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "HelpVue",
+}
+</script>
+
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ab! Sit
+      excepturi ad ipsam neque officia dolore porro, culpa accusantium
+      blanditiis ducimus. Debitis, ex culpa quam illo nulla in ipsa!
+    </p>
+  </div>
+</template>
