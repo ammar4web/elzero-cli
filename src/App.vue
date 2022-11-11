4ec3a5b2f85a2fb1d8309f9a7b2fcd40ae1d6889
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-11-11 20:48:35 +0300

    Header Episode 28, 29

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
+<script>
+import AppHeaderVue from "@/components/global/AppHeaderVue.vue"
+
+export default {
+  name: "App",
+  components: {
+    AppHeaderVue,
+  }
+}
+</script>
+
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/help">Help</router-link>
-    </nav>
+    <app-headerVue/>
     <router-view/>
   </div>
 </template>
---
+++ src/components/global/AppHeaderVue.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    name: "AppHeaderVue", 
+  }
+</script>
+
+<template>
+  <header>
+    <div class="logo">
+      <h1>Ammar<span>4web</span></h1>
+    </div>
+    <nav>
+      <!-- 
+        tag="li"
+        exact
+        <a>Home</a>
+      -->
+      <router-link to="/" tag="li" exact><a>Home</a></router-link>
+      <router-link to="/about" tag="li"><a>About</a></router-link>
+      <router-link to="/help" tag="li"><a>Help</a></router-link>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  h1 {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+  }
+}
+</style>
+
+<!-- global -->
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
